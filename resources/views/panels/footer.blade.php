6518b252059734a01702fe2ebf007fefbfa7b6d5
Akram Lazkanee <mlazkanee@accumed.ae>
2022-01-27 09:09:31 +0200

    Fixes

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -4,7 +4,7 @@
     <p class="clearfix mb-0">
     <span class="float-md-start d-block d-md-inline-block mt-25">&copy;
       <script>document.write(new Date().getFullYear())</script>,
-      <span class="d-none d-sm-inline-block">All rights Reserved, <a href="http://vegasds.com/">Vega SDS</a></span>
+      <span class="d-none d-sm-inline-block">All rights Reserved, DealGo</span>
     </span>
         <span class="float-md-end d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span>
     </p>
