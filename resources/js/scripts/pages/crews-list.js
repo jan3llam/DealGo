eba6168b8eb35ddad4d2e5f07789302ef84b15c0
Akram Lazkanee <mlazkanee@accumed.ae>
2022-02-02 10:57:57 +0200

    Fixes

--- resources/js/scripts/pages/crews-list.js
+++ resources/js/scripts/pages/crews-list.js
@@ -244,7 +244,9 @@ $(function () {
                     required: true
                 },
                 'file': {
-                    required: true
+                    required: function (element) {
+                        return parseInt($("#form_status").val()) === 1;
+                    }
                 },
                 'email': {
                     required: true
