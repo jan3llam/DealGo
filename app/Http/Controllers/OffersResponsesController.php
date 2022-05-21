5591f89ce54c98d7739129bacd29be15c9510660
Akram Lazkanee <mlazkanee@accumed.ae>
2022-05-22 01:10:08 +0300

    Bug fixing

--- app/Http/Controllers/OffersResponsesController.php
+++ app/Http/Controllers/OffersResponsesController.php
@@ -234,6 +234,7 @@ class OffersResponsesController extends Controller
 
             $paymentItem = new OfferResponsePayment;
             $paymentItem->value = $payment['value'];
+
             $paymentItem->date = Carbon::parse($payment['date'])->toDateString();
             $paymentItem->description = $payment['description'];
             $paymentItem->file = $fileName;
