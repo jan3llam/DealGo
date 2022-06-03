5d7839aef8a74e602bd86e077275150abf298bbc
Akram Lazkanee <mlazkanee@accumed.ae>
2022-06-03 12:24:10 +0300

    Bug fixes

---
+++ app/Http/Controllers/Api/TicketsController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Admin;
+use App\Models\Reply;
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+use Validator;
+
+class TicketsController extends Controller
+{
+    public function reply($id, Request $request)
+    {
+        $params = $request->all();
+        $validator = Validator::make($params, [
+            'reply' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->with('error', __('api.codes.missingParameters.message'))
+                ->withInput();
+        }
+
+        $item = Ticket::withTrashed()->where('id', $id)->first();
+
+        if ($item->status === 1 || $item->admin->id === auth('admins')->user()->id) {
+            $reply = new Reply;
+
+            $reply->ticket_id = $item->id;
+            $reply->author_id = auth('admins')->user()->id;
+            $reply->author_type = Admin::class;
+            $reply->text = $request->reply;
+
+            $item->replies()->save($reply);
+
+            $item->status = 2;
+            $item->admin_id = auth('admins')->user()->id;
+
+            $item->save();
+
+            return redirect()->back()
+                ->with('success', __('api.codes.success.message'))
+                ->withInput();
+        } else {
+            return redirect()->back()
+                ->with('error', __('api.codes.notAuthorized.message'))
+                ->withInput();
+        }
+
+    }
+
+    public function list_api(Request $request)
+    {
+
+        $data = [];
+        $search_clm = ['user.name', 'user.email', 'user.phone', 'user.contact_name',
+            'admin.name', 'admin.email', 'admin.phone', 'subject', 'description'];
+        $order_field = 'created_at';
+        $order_sort = 'desc';
+
+        $params = $request->all();
+        $query = Ticket::query();
+
+        $search_val = isset($params['search']) ? $params['search'] : null;
+        $sort_field = isset($params['order']) ? $params['order'] : null;
+        $page = isset($params['start']) ? $params['start'] : 0;
+        $filter_status = isset($params['status']) ? $params['status'] : 1;
+        $per_page = isset($params['length']) ? $params['length'] : 10;
+
+        if ($search_val) {
+            $query->where(function ($q) use ($search_clm, $search_val) {
+                foreach ($search_clm as $item) {
+                    $item = explode('.', $item);
+                    if (sizeof($item) == 3) {
+                        $q->orWhereHas($item[0], function ($qu) use ($item, $search_val) {
+                            $qu->whereHas($item[1], function ($que) use ($item, $search_val) {
+                                $que->where($item[2], 'like', '%' . $search_val . '%');
+                            });
+                        })->get();
+                    } elseif (sizeof($item) == 2) {
+                        $q->orWhereHas($item[0], function ($qu) use ($item, $search_val) {
+                            $qu->where($item[1], 'like', '%' . $search_val . '%');
+                        })->get();
+                    } elseif (sizeof($item) == 1) {
+                        $q->orWhere($item[0], 'like', '%' . $search_val . '%');
+                    }
+
+                }
+            });
+        }
+
+        if ($sort_field) {
+            $order_field = $sort_field;
+            $order_sort = $params['direction'];
+        }
+
+        if ($filter_status !== null) {
+            switch ($filter_status) {
+                case 1:
+                {
+                    $query->where('status', 1);
+                    break;
+                }
+                case 2:
+                {
+                    $query->where('status', 2);
+                    break;
+                }
+                case 3:
+                {
+                    $query->where('status', 3);
+                    break;
+                }
+                case 4:
+                {
+                    $query->onlyTrashed();
+                    break;
+                }
+            }
+        }
+
+        $total = $query->limit($per_page)->count();
+
+        $data['data'] = $query->skip(($page) * $per_page)
+            ->with([
+                'user' => function ($q) {
+                    $q->withTrashed();
+                },
+                'admin' => function ($q) {
+                    $q->withTrashed();
+                }
+            ])->take($per_page)->orderBy($order_field, $order_sort)->get();
+
+
+        $data['meta']['draw'] = $request->input('draw');
+        $data['meta']['total'] = $total;
+        $data['meta']['count'] = $data['data']->count();
+        $data['data'] = $data['data']->toArray();
+
+        return response()->success($data);
+    }
+}
