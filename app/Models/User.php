6f253199f8a8e1d36a0f35d56831742bc1162dd8
Akram Lazkanee <mlazkanee@accumed.ae>
2022-06-14 10:42:29 +0300

    Bug fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -100,9 +100,12 @@ class User extends Authenticatable implements JWTSubject
 
     public function getUserPaymentsSumAttribute()
     {
-        return $this->userable->contracts->reduce(function ($sum, $item) {
-            return $sum + $item->payments->sum('value');
-        }, 0);
+//        return $this->userable->contracts->reduce(function ($sum, $item) {
+//            return $sum + ($item->payments->sum('value'));
+//        }, 0);
+
+        return $this->userable->with('contracts.payments')->sum('value');
+
     }
 
     public function getUserNextPaymentAttribute()
