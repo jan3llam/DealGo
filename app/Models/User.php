596f233ddabbb22ce9812cec312158c66eae1812
Akram Lazkanee <mlazkanee@accumed.ae>
2022-06-14 10:43:31 +0300

    Bug fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -104,7 +104,9 @@ class User extends Authenticatable implements JWTSubject
 //            return $sum + ($item->payments->sum('value'));
 //        }, 0);
 
-        return $this->userable->with('contracts.payments')->sum('value');
+        return $this->userable->with(['contracts.payments' => function ($query) {
+            $query->select('value');
+        }])->sum('value');
 
     }
 
