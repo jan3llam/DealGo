4ea6759b9fe672358b8471cd53dd321021469077
Akram Lazkanee <mlazkanee@accumed.ae>
2022-06-14 10:07:20 +0300

    Bug fixes

--- app/Http/Controllers/Api/HomepageController.php
+++ app/Http/Controllers/Api/HomepageController.php
@@ -5,9 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Models\About;
 use App\Models\Advantage;
 use App\Models\Client;
+use App\Models\Contract;
+use App\Models\Owner;
 use App\Models\Post;
 use App\Models\Service;
+use App\Models\Shipment;
 use App\Models\Slider;
+use App\Models\Tenant;
+use App\Models\User;
 
 class HomepageController extends Controller
 {
@@ -20,6 +25,10 @@ class HomepageController extends Controller
         $data['clients'] = Client::withoutTrashed()->get();
         $data['about'] = About::withoutTrashed()->get();
         $data['slider'] = Slider::withoutTrashed()->get();
+        $data['statistics']['owners_count'] = User::whereHasMorph('userable', [Owner::class])->where('status', 1)->count();
+        $data['statistics']['tenants_count'] = User::whereHasMorph('userable', [Tenant::class])->where('status', 1)->count();
+        $data['statistics']['contracts_count'] = Contract::count();
+        $data['statistics']['shipments_count'] = Shipment::count();
         $data['posts'] = Post::withoutTrashed()->with('classification')->orderBy('created_at', 'desc')->limit(6)->get();
 
         return response()->success($data);
