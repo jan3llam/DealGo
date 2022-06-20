a0064de0cc53221d2561d8b1ed72b28ae12a8dfb
Akram Lazkanee <mlazkanee@accumed.ae>
2022-06-20 22:30:34 +0300

    Bug fixes

--- app/Http/Controllers/Api/RequestsController.php
+++ app/Http/Controllers/Api/RequestsController.php
@@ -29,7 +29,7 @@ class RequestsController extends Controller
         $query = ShippingRequest::whereHas('port_to')->whereHas('port_from')
             ->whereHas('tenant', function ($q) {
                 $q->whereHas('user');
-            })->with(['port_to', 'port_from', 'tenant.user', 'routes', 'goods_types'])
+            })->with(['port_to', 'port_from', 'tenant.user', 'routes', 'request_goods_types.good_type'])
             ->withCount(['responses' => function (Builder $q) {
                 $q->whereHas('vessels')->whereHas('request_goods_types');
             }]);
