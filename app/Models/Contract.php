a75de13d9d89ca0ba275aabd98422191ff2a06db
Akram Lazkanee <mlazkanee@accumed.ae>
2022-06-26 21:03:56 +0300

    Bug fixes

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -47,7 +47,7 @@ class Contract extends Model
     public function getGoodsTypesAttribute()
     {
         if ($this->origin instanceof OfferResponse) {
-            return $this->with('origin.goods_types.good_type')->get();
+            return $this->origin->goods_types;
         } elseif ($this->origin instanceof RequestResponse) {
             return $this->origin->request->goods_types;
         }
