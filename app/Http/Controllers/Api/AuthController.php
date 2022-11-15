11be1ae945421b7a40fb448037a279327af1c2b4
Akram Lazkanee <mlazkanee@accumed.ae>
2022-11-15 20:38:30 +0300

    Bug fixes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -334,7 +334,7 @@ class AuthController extends Controller
             $code->save();
             $user->save();
         } else {
-            dd($code->created_at->diffInMinutes(now()) < intval(env('SMS_VALID_MINUTES')));
+//            dd($code->created_at->diffInMinutes(now()) < intval(env('SMS_VALID_MINUTES')));
             return response()->error('codeNotValid');
         }
 
