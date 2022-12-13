f542813b2cc8903707cbcb1ec1ff121bc87a711e
Akram Lazkanee <mlazkanee@accumed.ae>
2022-12-13 14:39:33 +0300

    Bug fixes

--- resources/views/content/offers-list.blade.php
+++ resources/views/content/offers-list.blade.php
@@ -257,6 +257,10 @@
         $('#port_from').select2({
             ajax: {
                 url: '/api/admin/ports/list',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                    'Authorization': 'Bearer ' + $('meta[name="api-token"]').attr('content')
+                },
                 data: function (params) {
                     console.log(params);
                     var query = {
